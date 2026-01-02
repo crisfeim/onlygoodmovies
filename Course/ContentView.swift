5f89f9912bb3641da77081354337d7ada5027be6
cristian.rojas@live.fr <cristian.rojas@live.fr>
2026-01-02 23:08:48 +0000

    Initial Commit
    FossilOrigin-Name: d15259ba344f1eb4f9c6b9ca749bf9e2b213c0af367781b8b46be4df95def96d

---
+++ Course/ContentView.swift
@@ -0,0 +1,19 @@
+// © 2026  Cristian Felipe Patiño Rojas. Created on 3/1/26.
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Course/CourseApp.swift
@@ -0,0 +1,12 @@
+// © 2026  Cristian Felipe Patiño Rojas. Created on 3/1/26.
+
+import SwiftUI
+
+@main
+struct CourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
