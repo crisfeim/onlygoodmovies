59cafaf6861ebe38b8a14262f24d340dbaebfe03
cristian.rojas@live.fr <cristian.rojas@live.fr>
2026-01-13 17:50:35 +0000

    Movee only good movies to host
    FossilOrigin-Name: c7ef3ff07550f205f3afe9893f62a6434dc4605ac46f84901441a0f9676ee87d

--- Core/MoviesApi/OnlyGoodMoviesApi.swift
+++
@@ -1,9 +0,0 @@
-// © 2026  Cristian Felipe Patiño Rojas. Created on 3/1/26.
-
-
-import Foundation
-import SwiftUI
-
-public enum OnlyGoodMoviesApi {
-    public static let movies = URL(string: "https://crisfe.im/apis/only-good-movies/v1")!
-}
