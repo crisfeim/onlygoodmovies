26ec27bde11f1384792ab9d9016085e38d812b29
cristian.rojas@live.fr <cristian.rojas@live.fr>
2026-01-14 15:05:58 +0000

    Add make sut factory
    FossilOrigin-Name: f8d743492603ceb0b670d296fe670235e707ba3839d9b817484cbee83433578c

--- MoviesiOSUITests/AsyncCachedImageTests.swift
+++ MoviesiOSUITests/AsyncCachedImageTests.swift
@@ -11,7 +11,7 @@ struct AsyncImageWithCache<Image: AnyObject> {
     
     let client: HTTPClient
     let mapper: (Data) -> Image?
-  
+    
     
     func loadImage() async {
         if let url, let (d, _) = try? await client(url), let image = mapper(d) {
@@ -21,36 +21,41 @@ struct AsyncImageWithCache<Image: AnyObject> {
 }
 
 class AsyncImageWithCacheTests: XCTestCase {
+    class Dummy {}
+    
     func test_loadImageDoesntCachesDataOnRequestError() async {
-        class Dummy {}
-        let cache = NSURLCache<Dummy>(countLimit: 1)
-        
         let url = URL(string: "https://www.google.com")!
-        
-        let sut = AsyncImageWithCache<Dummy>(
-            cache: cache,
-            url: url,
-            client: { _ in throw NSError(domain: "any-error", code: 0) },
-            mapper: { _ in Dummy() }
-        )
-
-       
+        let (sut, cache) = makeSUT(url: url, client: { _ in throw anyError() })
         await sut.loadImage()
         XCTAssertNil(cache.get(url))
     }
     
+
     func test_loadImageCachesDataOnRequesSuccess() async {
-        class Dummy {}
-        let cache = NSURLCache<Dummy>(countLimit: 1)
         let url = URL(string: "https://www.google.com")!
-        let sut = AsyncImageWithCache<Dummy>(
-            cache: cache,
-            url: url,
-            client: { _ in (Data(), HTTPURLResponse()) },
-            mapper: { _ in Dummy() }
-        )
-       
+        let (sut, cache) = makeSUT(url: url, client: { _ in (Data(), HTTPURLResponse()) })
         await sut.loadImage()
         XCTAssertNotNil(cache.get(url))
     }
+    
+    func makeSUT(url: URL, client: @escaping HTTPClient, mapper: @escaping (Data) -> Dummy? = anyMapper()) -> (sut: AsyncImageWithCache<Dummy>, cache: NSURLCache<Dummy>) {
+        let cache = NSURLCache<Dummy>(countLimit: 1)
+        let sut = AsyncImageWithCache<Dummy>(
+            cache: cache,
+            url: url,
+            client: client,
+            mapper: mapper
+        )
+        
+        return (sut: sut, cache: cache)
+    }
+}
+
+
+fileprivate func anyMapper() -> (Data) -> AsyncImageWithCacheTests.Dummy? {
+    { _ in .init() }
+}
+
+fileprivate func anyError() -> Error {
+    NSError(domain: "any-error", code: 0)
 }
