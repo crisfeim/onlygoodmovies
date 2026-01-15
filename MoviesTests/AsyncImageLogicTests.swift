d96e50b9a5b2763b23d0de3607fb04cdd4e7244d
cristian.rojas@live.fr <cristian.rojas@live.fr>
2026-01-15 18:42:34 +0000

    `load` does not deliver image on empty store
    FossilOrigin-Name: 299e9aaf98bb72118dd00467558d48973917a80a494ed565b7397dfda87cf840

--- MoviesTests/AsyncImageLogicTests.swift
+++ MoviesTests/AsyncImageLogicTests.swift
@@ -6,12 +6,20 @@ class AsyncImageLogicTests: XCTestCase {
     
     struct AsyncImageLogic {
         @Binding var image: Image?
+        let store: (URL) -> Image?
         func load() {}
     }
     
     func test_loadDoesntDeliverImageOnInit() {
         let binding = makeBinding(Optional<Image>.none)
-        let _ = AsyncImageLogic(image: binding)
+        let _ = AsyncImageLogic(image: binding) { _  in nil } 
+        XCTAssertNil(binding.wrappedValue)
+    }
+    
+    func test_loadDoesntDeliverImageOnEmptyStore() {
+        let binding = makeBinding(Optional<Image>.none)
+        let sut = AsyncImageLogic(image: binding, store: { _ in nil })
+        sut.load()
         XCTAssertNil(binding.wrappedValue)
     }
 }
