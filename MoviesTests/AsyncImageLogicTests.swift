5cba6dab7061ce8aa7b8f52dead3d2763536daa5
cristian.rojas@live.fr <cristian.rojas@live.fr>
2026-01-15 18:47:35 +0000

    `load` does not deliver image if url does not match when non empty store
    FossilOrigin-Name: 5a26fc17c7b743e2e29b836ff11b903c9ea01abbaa3936051af678b023641aa2

--- MoviesTests/AsyncImageLogicTests.swift
+++ MoviesTests/AsyncImageLogicTests.swift
@@ -5,6 +5,7 @@ import SwiftUI
 class AsyncImageLogicTests: XCTestCase {
     
     struct AsyncImageLogic {
+        let url: URL?
         @Binding var image: Image?
         let store: (URL) -> Image?
         func load() {}
@@ -12,14 +13,28 @@ class AsyncImageLogicTests: XCTestCase {
     
     func test_loadDoesntDeliverImageOnInit() {
         let binding = makeBinding(Optional<Image>.none)
-        let _ = AsyncImageLogic(image: binding) { _  in nil } 
+        let _ = AsyncImageLogic(url: anyURL(), image: binding) { _  in nil }
         XCTAssertNil(binding.wrappedValue)
     }
     
     func test_loadDoesntDeliverImageOnEmptyStore() {
         let binding = makeBinding(Optional<Image>.none)
-        let sut = AsyncImageLogic(image: binding, store: { _ in nil })
+        let sut = AsyncImageLogic(url: anyURL(), image: binding, store: { _ in nil })
         sut.load()
         XCTAssertNil(binding.wrappedValue)
     }
+    
+    func test_loadDoesntDeliverImageIfURLDoesntMatchWhenNonEmptyStore() {
+        let binding = makeBinding(Optional<Image>.none)
+        let storage: [URL: Image] = [URL(string: "https://some-stored-url.com")!: Image("")]
+        let sut = AsyncImageLogic(url: anyURL(), image: binding) { url in
+            storage[url]
+        }
+        sut.load()
+        XCTAssertNil(binding.wrappedValue)
+    }
+}
+
+fileprivate func anyURL() -> URL? {
+    URL(string: "https://any-url.com")
 }
