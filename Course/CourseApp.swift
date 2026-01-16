7c1b3457218f905debf6f318db221e7db5c3c73f
cristian.rojas@live.fr <cristian.rojas@live.fr>
2026-01-16 00:24:43 +0000

    Add urlcached session and rething operators because conflcits
    FossilOrigin-Name: 6b16d81c725475764be46e784d8d30115087e44fdf5b87decabeaa0e659f1b30

--- Course/CourseApp.swift
+++ Course/CourseApp.swift
@@ -4,34 +4,47 @@ import SwiftUI
 import Movies
 import MoviesiOSUI
 
+
 fileprivate let httpClient   = URLSessionHTTPClient(URLSession.shared)
 fileprivate let remoteLoader = RemoteMoviesLoader(OnlyGoodMoviesApi.movies, httpClient)
+
+nonisolated let imagesCachedSession = URLCachedSession(URLCache(
+    memoryCapacity: 50*1024*1024,
+    diskCapacity: 150*1024*1024,
+    diskPath: "images"
+))
+
 fileprivate let imagesLoader: @Sendable (URL) async throws -> Image? = { url in
-    let (d, _) = try await httpClient(url)
+    let d = try await imagesCachedSession.download(url)
     return UIImage(data: d).map { Image(uiImage: $0) }
+}
+
+fileprivate let imagesStore: @Sendable (URL) -> Image? = { url in
+    imagesCachedSession.retrieve(url).flatMap { data in
+        UIImage(data: data).map { Image(uiImage: $0) }
+    }
 }
 
 @main struct CourseApp: App {
     var body: some Scene {
         WindowGroup {
-             MoviesUIComposer(loader: remoteLoader~>withRetry|2)
+            MoviesUIComposer(loader: remoteLoader --> withRetry .= 2)
                 .environment(\.imagesLoader, imagesLoader)
         }
     }
 }
 
-// Composition Decorators & Functional helpers
-infix operator ~>: AdditionPrecedence
-func ~> <T, V>(lhs: T, rhs: (T) -> V) -> V { rhs(lhs) }
 
-infix operator |: MultiplicationPrecedence
-func | <T, U, V>(lhs: @escaping (T, U) -> V, rhs: U) -> (T) -> V {
+infix operator -->: AdditionPrecedence
+nonisolated func --><T, V>(lhs: T, rhs: (T) -> V) -> V { rhs(lhs) }
+
+infix operator .=: MultiplicationPrecedence
+func .= <T, U, V>(lhs: @escaping (T, U) -> V, rhs: U) -> (T) -> V {
     return { T in lhs(T, rhs) }
 }
 
-
-fileprivate typealias Loader<T: Sendable> = @Sendable () async throws -> T
-fileprivate func withRetry<T>(_ load: @escaping Loader<T>, attempts: Int = 3) ->  Loader<T> {
+ typealias Loader<T: Sendable> = @Sendable () async throws -> T
+func withRetry<T>(_ load: @escaping Loader<T>, attempts: Int = 3) ->  Loader<T> {
     {
         var lastError: Error?
         for _ in 0..<attempts {
---
+++ Course/URLCachedSession.swift
@@ -0,0 +1,21 @@
+// © 2026  Cristian Felipe Patiño Rojas. Created on 16/1/26.
+import Foundation
+
+typealias CachedSession = (
+    download: @Sendable (URL) async throws -> Data,
+    retrieve: @Sendable (URL) -> Data?
+)
+
+var URLCachedSession: @Sendable (URLCache) -> CachedSession {
+    { cache in
+        let config = URLSessionConfiguration.default
+        config.requestCachePolicy = .returnCacheDataElseLoad
+        config.urlCache = cache
+        let session = URLSession(configuration: config)
+    
+        return (
+            download: { try await session.data(from: $0).0 },
+            retrieve: { URLRequest(url: $0) --> cache.cachedResponse(for:) --> { $0?.data }}
+        )
+    }
+}
