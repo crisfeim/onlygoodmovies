655a13ee4535c636a5fd88c5eda0e29595a2dce2
cristian.rojas@live.fr <cristian.rojas@live.fr>
2026-01-18 03:36:32 +0000

    Simplify `MoviesListComposer` by renaming `AsyncThumbnail` generic to  `Thumbnail` & remove `state` from init
    FossilOrigin-Name: f86a43a3f95a73cadcaa2bfed4f2b55b296cf9bf291e183d5ddcdda1ae86e3e7

--- Course/MoviesListComposer.swift
+++ Course/MoviesListComposer.swift
@@ -4,23 +4,21 @@ import Movies
 import MoviesiOSUI
 import SwiftUI
 
-struct MoviesListComposer<AsyncThumbnail: View>: View {
+struct MoviesListComposer<Thumbnail: View>: View {
     @State var state  = MoviesState()
     @State var config = MoviesConfig()
     
     let loader: MoviesLoader
-    let cell: (Movie) -> MovieCell<AsyncThumbnail>
+    let cell: (Movie) -> MovieCell<Thumbnail>
     
     var presenter: MoviesPresenter {
         .init($state, $config, loader: loader)
     }
     
     init(
-        state: MoviesState = MoviesState(),
         loader: @escaping MoviesLoader,
-        thumbnailProvider: @escaping (URL?) -> AsyncThumbnail,
+        thumbnailProvider: @escaping (URL?) -> Thumbnail,
     ) {
-        self.state = state
         self.loader = loader
         self.cell =  { MovieCell(movie: $0, thumbnailProvider: thumbnailProvider) }
     }
