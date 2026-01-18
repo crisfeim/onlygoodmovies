857a430db5b57e640e1e62957b6d8895fbf2fab8
cristian.rojas@live.fr <cristian.rojas@live.fr>
2026-01-18 02:51:08 +0000

    Move composition preview to its own file
    FossilOrigin-Name: 7fc883b93a13373a0f04d039b3ff032b35cd82541a6e69607e9ced2df22726ca

--- Course/MoviesListComposer.swift
+++ Course/MoviesListComposer.swift
@@ -31,39 +31,3 @@ struct MoviesListComposer<AsyncThumbnail: View>: View {
         .task(logic.load)
     }
 }
-
-
-// Previews
-
-#Preview("App") {
-    @Previewable @State var id = UUID()
-    
-    var shouldFail = false
-    let movie = Movie(
-        id: "17",
-        title: "The Passion of the Christ",
-        posterURL: "https://crisfe.im/apis/only-good-movies/passionofchrist.png",
-        releaseYear: 2004
-    )
-  
-    MoviesListComposer(
-        loader: {@MainActor in
-        try await Task.sleep(for: .seconds(2))
-        
-        if shouldFail {
-            shouldFail = false
-            throw NSError(domain: "any-error", code: 0)
-        } else {
-            shouldFail = true
-            return Array(0...10).map {_ in movie }
-        }
-        },
-        thumbnailProvider: AsyncImage<MovieThumbnail>.make
-    )
-    .id(id)
-    .toolbar {
-        ToolbarItem(placement: .bottomBar) {
-            Button("Reload") { id = UUID() }
-        }
-    }
-}
---
+++ Course/MoviesListComposition_Preview.swift
@@ -0,0 +1,39 @@
+// © 2026  Cristian Felipe Patiño Rojas. Created on 18/1/26.
+import Movies
+import MoviesiOSUI
+import SwiftUI
+
+// Previews
+
+#Preview("App") {
+    @Previewable @State var id = UUID()
+    
+    var shouldFail = false
+    let movie = Movie(
+        id: "17",
+        title: "The Passion of the Christ",
+        posterURL: "https://crisfe.im/apis/only-good-movies/passionofchrist.png",
+        releaseYear: 2004
+    )
+  
+    MoviesListComposer(
+        loader: {@MainActor in
+        try await Task.sleep(for: .seconds(2))
+        
+        if shouldFail {
+            shouldFail = false
+            throw NSError(domain: "any-error", code: 0)
+        } else {
+            shouldFail = true
+            return Array(0...10).map {_ in movie }
+        }
+        },
+        thumbnailProvider: AsyncImage<MovieThumbnail>.make
+    )
+    .id(id)
+    .toolbar {
+        ToolbarItem(placement: .bottomBar) {
+            Button("Reload") { id = UUID() }
+        }
+    }
+}
