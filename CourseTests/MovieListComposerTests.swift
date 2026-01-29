092c0e7d372b7126ee3b107c83255f703e306499
cristian.rojas@live.fr <cristian.rojas@live.fr>
2026-01-29 19:31:05 +0000

    Make helper not throwable (failible through xcfail) & add helper for stream creation from sendable arrays
    FossilOrigin-Name: 66a846abe8969722ee10bb19adc635a98f1d0b290bf40465cf8bd40f5f28f14e

--- CourseTests/MovieListComposerTests.swift
+++ CourseTests/MovieListComposerTests.swift
@@ -9,20 +9,10 @@ import Movies
 final class MovieListComposerTests: XCTestCase {
     func test_moviesAreLoadedAfterRendered() async throws {
         let expectedMovies = [anyMovie()]
-        
-        let loader: MoviesLoader = { @Sendable in
-            AsyncThrowingStream { continuation in
-                for movie in expectedMovies {
-                    continuation.yield(movie)
-                }
-                continuation.finish()
-            }
-        }
-//        
-        let composer = MovieListComposer(loader: loader) { _ in Text("any thumbnail") }
+        let composer = MovieListComposer<Text>(loader: expectedMovies.stream) { _ in Text("any thumbnail") }
         TestApp.shared.setView(composer)
         
-        for try await (index, view) in MovieListComposer<Text>.bodyEvaluations().prefix(2).timeout(1) {
+        for await (index, view) in MovieListComposer<Text>.bodyEvaluations().prefix(2).timeout(1) {
             switch index {
             case 0: XCTAssertEqual(view.state.movies, [])
             case 1: XCTAssertEqual(view.state.movies, expectedMovies)
@@ -32,37 +22,54 @@ final class MovieListComposerTests: XCTestCase {
     }
 }
 
+fileprivate extension Array where Element: Sendable {
+    var stream: @Sendable () -> AsyncThrowingStream<Element, Error> {
+        {
+            AsyncThrowingStream { continuation in
+                for item in self {
+                    continuation.yield(item)
+                }
+                continuation.finish()
+            }
+        }
+    }
+}
+
 fileprivate func anyMovie() -> Movie {
     Movie(id: "0", title: "some title", posterURL: "someposterurl", releaseYear: 2000)
 }
 
-private struct Wrapper<Element, Failure>: @unchecked Sendable {
+fileprivate struct Wrapper<Element, Failure>: @unchecked Sendable {
     let value: any AsyncSequence<Element, Failure>
 }
 
-extension AsyncSequence where Element: Sendable {
-    func timeout(_ seconds: TimeInterval) -> AsyncThrowingStream<Element, Error> {
+fileprivate extension AsyncSequence where Element: Sendable {
+    func timeout( _ seconds: TimeInterval, file: StaticString = #filePath, line: UInt = #line) -> AsyncStream<Element> {
         let wrapped = Wrapper(value: self)
-        return AsyncThrowingStream { continuation in
-            let timeoutTask = Task {
-                try? await Task.sleep(for: .seconds(seconds))
-                continuation.finish(throwing: CancellationError())
-            }
+        return AsyncStream { continuation in
             
-            Task { @Sendable in
+            let iterationTask = Task { @Sendable in
                 do {
                     for try await element in wrapped.value {
                         continuation.yield(element)
                     }
-                    timeoutTask.cancel()
                     continuation.finish()
                 } catch {
-                    timeoutTask.cancel()
-                    continuation.finish(throwing: error)
+                    continuation.finish()
+                }
+            }
+            
+            let timeoutTask = Task { [iterationTask] in
+                try? await Task.sleep(for: .seconds(seconds))
+                if !Task.isCancelled {
+                    XCTFail("Timeout reached", file: file, line: line)
+                    iterationTask.cancel()
+                    continuation.finish()
                 }
             }
             
             continuation.onTermination = { _ in
+                iterationTask.cancel()
                 timeoutTask.cancel()
             }
         }
