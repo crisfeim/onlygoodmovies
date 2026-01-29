f77378c9907556ea95f10eef2b3bc97c4ff70521
cristian.rojas@live.fr <cristian.rojas@live.fr>
2026-01-29 19:17:59 +0000

    Add timeout so test fails if we don't get the expected evaluations
    FossilOrigin-Name: e14200178d7b22d807354eb598b46f1bd2765d7c303e5937b85a9d5f409dd05e

--- CourseTests/MovieListComposerTests.swift
+++ CourseTests/MovieListComposerTests.swift
@@ -7,7 +7,7 @@ import Movies
 
 @MainActor
 final class MovieListComposerTests: XCTestCase {
-    func test_moviesAreLoadedAfterRendered() async {
+    func test_moviesAreLoadedAfterRendered() async throws {
         let expectedMovies = [anyMovie()]
         
         let loader: MoviesLoader = { @Sendable in
@@ -18,11 +18,11 @@ final class MovieListComposerTests: XCTestCase {
                 continuation.finish()
             }
         }
-        
+//        
         let composer = MovieListComposer(loader: loader) { _ in Text("any thumbnail") }
         TestApp.shared.setView(composer)
         
-        for await (index, view) in MovieListComposer<Text>.bodyEvaluations().prefix(2) {
+        for try await (index, view) in MovieListComposer<Text>.bodyEvaluations().prefix(2).timeout(1) {
             switch index {
             case 0: XCTAssertEqual(view.state.movies, [])
             case 1: XCTAssertEqual(view.state.movies, expectedMovies)
@@ -34,4 +34,37 @@ final class MovieListComposerTests: XCTestCase {
 
 fileprivate func anyMovie() -> Movie {
     Movie(id: "0", title: "some title", posterURL: "someposterurl", releaseYear: 2000)
+}
+
+private struct Wrapper<Element, Failure>: @unchecked Sendable {
+    let value: any AsyncSequence<Element, Failure>
+}
+
+extension AsyncSequence where Element: Sendable {
+    func timeout(_ seconds: TimeInterval) -> AsyncThrowingStream<Element, Error> {
+        let wrapped = Wrapper(value: self)
+        return AsyncThrowingStream { continuation in
+            let timeoutTask = Task {
+                try? await Task.sleep(for: .seconds(seconds))
+                continuation.finish(throwing: CancellationError())
+            }
+            
+            Task { @Sendable in
+                do {
+                    for try await element in wrapped.value {
+                        continuation.yield(element)
+                    }
+                    timeoutTask.cancel()
+                    continuation.finish()
+                } catch {
+                    timeoutTask.cancel()
+                    continuation.finish(throwing: error)
+                }
+            }
+            
+            continuation.onTermination = { _ in
+                timeoutTask.cancel()
+            }
+        }
+    }
 }
